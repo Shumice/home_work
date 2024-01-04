f91bae6ea4f1f14e31a16d227a5d14dd19849d48
Shumice <courage1980@mail.ru>
2024-01-04 20:33:41 +0300

    try2

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -7,11 +7,11 @@ import (
 )
 
 func PrintStaff(staff []types.Employee) {
-	var str string
+	//var str string
 	for i := 0; i < len(staff); i++ {
-		str := fmt.Sprintf("UserID: %d; Age: %d; Name: %s; DepartmentID: %d; ",
-			staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
-		fmt.Println(str)
+		//str := fmt.Sprintf("UserID: %d; Age: %d; Name: %s; DepartmentID: %d; ",
+		//	staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
+		fmt.Println(staff[i])
 	}
-	fmt.Println(str)
+	//fmt.Println(str)
 }
--- hw02_fix_app/reader/pkg.go
+++ hw02_fix_app/reader/pkg.go
@@ -13,6 +13,7 @@ func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
+		return nil, err
 	}
 
 	bytes, err := io.ReadAll(f)
@@ -24,6 +25,10 @@ func ReadJSON(filePath string, limit int) ([]types.Employee, error) {
 	var data []types.Employee
 
 	err = json.Unmarshal(bytes, &data)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return nil, err
+	}
 
 	res := data
 
