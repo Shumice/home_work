d09bcf7b82130a4ea0bc090b113d65c0d782679a
Shumice <courage1980@mail.ru>
2024-01-05 19:24:37 +0300

    fix_goWork

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -7,11 +7,7 @@ import (
 )
 
 func PrintStaff(staff []types.Employee) {
-	//var str string
 	for i := 0; i < len(staff); i++ {
-		//str := fmt.Sprintf("UserID: %d; Age: %d; Name: %s; DepartmentID: %d; ",
-		//	staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
 		fmt.Println(staff[i])
 	}
-	//fmt.Println(str)
 }
